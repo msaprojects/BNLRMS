f5a93f71fb704f0b21161cd37aa5e680ee60968d
msaprojects <msadev96@gmail.com>
2021-11-23 09:21:55 +0700

    update url

--- client/rmsvue/src/utils/services/penggunaService.js
+++ client/rmsvue/src/utils/services/penggunaService.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
-
+var url = 'http://server.bnl.id:9990/api/v1/'
 export async function getAllPengguna(){
     console.log('load here..?')
-    const response = await axios.get('http://server.bnl.id:9990/api/v1/pengguna');
+    const response = await axios.get(url+'pengguna');
     console.log('Data?',response.data.data);
     return response.data.data;
 }
